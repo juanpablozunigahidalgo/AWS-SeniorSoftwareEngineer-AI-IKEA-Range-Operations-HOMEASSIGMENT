70c45aa6cb42d6a03dc118ae2fec2e9909080019
JuanPabloZ <juanpablo.zunigah@gmail.com>
2025-12-16 20:59:32 +0100

    changes

---
+++ src/data-source.ts
@@ -0,0 +1,31 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Product } from "./models/Products"; // Asegúrate que el nombre coincida con tu archivo
+import { envs } from "./config/envs"; // <--- IMPORTAMOS LA NUEVA CONFIG
+
+// LOG
+console.log("🕵️ DATOS DE CONEXIÓN:", {
+    host: envs.db.host,
+    user: envs.db.username,
+    pass: envs.db.password, // ¿Qué contraseña sale aquí?
+    port: envs.db.port
+});
+// 
+
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: envs.db.host,
+    port: envs.db.port,
+    username: envs.db.username,
+    password: envs.db.password,
+    database: envs.db.name,
+    synchronize: true,
+    logging: false,
+    entities: [Product],
+    subscribers: [],
+    migrations: [],
+    ssl: envs.db.host === "localhost" || envs.db.host === "127.0.0.1" 
+         ? false 
+         : { rejectUnauthorized: false },
+});
